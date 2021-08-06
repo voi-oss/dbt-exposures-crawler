57faab45d1b5554691c37e97ba6f2334ed2ed26b
Fernando Brito <fernando.brito@voiapp.io>
2021-08-06 20:16:44 +0200

    feat: GitHub Action to publish the package (#3)

--- setup.py
+++ setup.py
@@ -9,15 +9,19 @@ long_description = (current_folder / 'README.md').read_text(encoding='utf-8')
 # Based on: https://packaging.python.org/tutorials/packaging-projects/
 setup(
     name='dbt-exposures-crawler',
-    version='0.0.1',
+    use_scm_version={
+        "write_to": "src/exposurescrawler/_version.py",
+        "write_to_template": '__version__ = "{version}"\n',
+        "local_scheme": "no-local-version",
+    },
     description='Extracts information from different systems and convert them to dbt exposures',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/voi-oss',
+    url='https://github.com/voi-oss/dbt-exposures-crawler',
     author='Voi Technology AB',
+    license='Apache License, Version 2.0',
     classifiers=[
         'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: Apache Software License 2.0 (Apache-2.0)',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3 :: Only',
@@ -25,6 +29,7 @@ setup(
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
     python_requires='>=3.9, <4',
+    setup_requires=['wheel', 'setuptools_scm'],
     install_requires=[
         'click ~= 8.0.1',
         'python-slugify ~= 4.0.1',
