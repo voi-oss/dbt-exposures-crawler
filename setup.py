f663ee523625d325a8e95b617c60b4cdc1b082fc
Fernando Brito <fernando.brito@voiapp.io>
2021-08-06 20:03:09 +0200

    feat: GitHub Action to publish the package

--- setup.py
+++ setup.py
@@ -9,7 +9,11 @@ long_description = (current_folder / 'README.md').read_text(encoding='utf-8')
 # Based on: https://packaging.python.org/tutorials/packaging-projects/
 setup(
     name='dbt-exposures-crawler',
-    use_scm_version=True,
+    use_scm_version={
+        "write_to": "src/exposurescrawler/_version.py",
+        "write_to_template": '__version__ = "{version}"\n',
+        "local_scheme": "no-local-version",
+    },
     description='Extracts information from different systems and convert them to dbt exposures',
     long_description=long_description,
     long_description_content_type='text/markdown',
