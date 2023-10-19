44f210f9276cadaae868b281d7d45837cc7322ae
Sandhya Yadav <sandhya.yadav@MacBook-Pro.local>
2023-10-19 11:34:16 -0400

    new

--- src/exposurescrawler/crawlers/tableau.py
+++ src/exposurescrawler/crawlers/tableau.py
@@ -4,6 +4,7 @@ import os
 from typing import Collection, List
 from unicodedata import name
 import pathlib
+from ruamel.yaml import YAML
 
 
 from exposurescrawler.tableau.models import WorkbookReference, WorkbookModelsMapping
@@ -168,17 +169,33 @@ def tableau_crawler(
             continue
 
         exposure = DbtExposure.from_tableau_workbook(dbt_package_name, workbook, owner, found)
-        #print(exposure)
+        print(exposure)
 
-        #import json
-        #test_raw = exposure.to_dict()
-        #test_name = test_raw.get("name")
-        #new_name = test_name.rsplit("_",1)[0]
-        #print(new_name)
-        #file_name = '/Users/sandhya.yadav/Desktop/dbt/dbt2/loom-dbt/target/' + new_name + '.txt'
+        import json
+        test_raw = exposure.to_dict()
+        test_name = test_raw.get("name")
+        new_name = test_name.rsplit("_",1)[0]
+        print(new_name)
+        file_name = '../../main/models/exposures/' + new_name + '.yml'
         #print(file_name)
-        #f = open(file_name, "a")
+        ff = open(file_name, "w+")
         #f.write(str(exposure.to_dict()))
+
+        yaml = YAML()
+
+
+        depends = list(('ref(''' + "'" + k.split('.')[-1] + "'" + ')''').lower() for k in set([model['unique_id'] for model in found]))
+        bb = exposure.to_dict()
+        l = {'name','type','url','description','owner'}
+        bc = {key: bb[key] for key in l if key in bb}
+        yamlData = {
+        'version': 2,
+        'exposures': [{'name': bc.get('name').rsplit("_",1)[0].lower(),'type': bc.get('type').lower(),'url':bc.get('url').lower(),'description':bc.get('description').lower(),'owner':bc.get('owner'), 'depends_on': depends}]
+        }
+        yaml.indent(sequence=4, offset=2)
+        yaml.dump(yamlData, ff)
+
+
         #f.write('\n')
         #f.close()
         manifest.add_exposure(exposure, found)
